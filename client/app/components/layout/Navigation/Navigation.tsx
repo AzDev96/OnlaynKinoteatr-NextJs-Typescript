ade0aca9400ddbcf7171e9936d5b00e2c33d434e
Azizbek <46714958+Azizbek1@users.noreply.github.com>
2023-01-28 22:32:38 +0500

    Laoyu 1 - video End
    Laoyu 1 - video End

---
+++ client/app/components/layout/Navigation/Navigation.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+import Logo from './Logo'
+import MenuContainer from './MenuContainer/MenuContainer'
+
+import styles from './Navigation.module.scss'
+
+const Navigation: FC = () => {
+	return (
+		<div className={styles.navigation}>
+			Navigate
+		</div>
+	)
+}
+
+export default Navigation
---
+++ client/app/components/screens/home/Home.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import Layout from '../../layout/Layout'
+
+const Home: FC = () => {
+	return (
+		<Layout>
+			<h1>Home Page</h1>
+		</Layout>
+		
+	)
+}
+
+export default Home
