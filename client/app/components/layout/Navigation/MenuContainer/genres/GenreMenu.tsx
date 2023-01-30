825e096c6f6d9c22f7b0cb8338a88c334cbb77de
Azizbek1 <azizochilov569@gmail.com>
2023-01-30 17:40:53 +0500

    cleint
    cleint

--- client/app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
+++ client/app/components/layout/Navigation/MenuContainer/genres/GenreMenu.tsx
@@ -1,24 +1,9 @@
-import SkeletonLoader from '@/ui/skeleton-loader/SkeletonLoader'
-
-import Menu from '../menu/Menu'
-
-import { usePopularGenres } from './usePopularGenres'
-
 const GenreMenu = () => {
-	const { isLoading, data } = usePopularGenres()
-
-	return isLoading ? (
-		<div className="mx-11 mb-6">
-			<SkeletonLoader count={5} className="h-7 mt-6" />
+	return  (
+		<div>
+			GenreMenu
 		</div>
-	) : (
-		<Menu
-			menu={{
-				title: 'Popular genres',
-				items: data || [],
-			}}
-		/>
-	)
+	) 
 }
 
 export default GenreMenu
